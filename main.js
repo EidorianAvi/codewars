33daa3ded3362d1d05f671b609ca9fb664930985
EidorianAvi <reneavila1993@gmail.com>
2020-06-26 09:59:50 -0600

    6/26/2020

--- main.js
+++ main.js
@@ -12,3 +12,35 @@ Test.assertEquals(positiveSum([1, -2, 3, 4, 5]), 13);
 Test.assertEquals(positiveSum([]), 0);
 Test.assertEquals(positiveSum([-1, -2, -3, -4, -5]), 0);
 Test.assertEquals(positiveSum([-1, 2, 3, 4, -5]), 9);
+
+
+// 6/26/2020
+
+// Given a string, capitalize the letters that occupy even indexes and odd indexes separately, and return as shown below.Index 0 will be considered even.
+
+// For example, capitalize("abcdef") = ['AbCdEf', 'aBcDeF'].See test cases for more examples.
+
+function capitalize(s) {
+  let output = []
+  let split = s.split('');
+
+  let evens = split.map((letter, i) => {
+    if (i % 2 == 0) {
+      return letter.toUpperCase();
+    } else {
+      return letter.toLowerCase();
+    }
+  });
+  output.push(evens.join(''));
+
+  let odds = split.map((letter, i) => {
+    if (i % 2 != 0) {
+      return letter.toUpperCase();
+    } else {
+      return letter.toLowerCase();
+    }
+  });
+  output.push(odds.join(''));
+
+  return output
+}
