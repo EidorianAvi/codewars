371d1cf1ba053d9474b8a652002b19411aa1948f
EidorianAvi <reneavila1993@gmail.com>
2020-06-27 10:31:59 -0600

    6/27/20

--- main.js
+++ main.js
@@ -13,7 +13,6 @@ Test.assertEquals(positiveSum([]), 0);
 Test.assertEquals(positiveSum([-1, -2, -3, -4, -5]), 0);
 Test.assertEquals(positiveSum([-1, 2, 3, 4, -5]), 9);
 
-
 // 6/26/2020
 
 // Given a string, capitalize the letters that occupy even indexes and odd indexes separately, and return as shown below.Index 0 will be considered even.
@@ -21,8 +20,8 @@ Test.assertEquals(positiveSum([-1, 2, 3, 4, -5]), 9);
 // For example, capitalize("abcdef") = ['AbCdEf', 'aBcDeF'].See test cases for more examples.
 
 function capitalize(s) {
-  let output = []
-  let split = s.split('');
+  let output = [];
+  let split = s.split("");
 
   let evens = split.map((letter, i) => {
     if (i % 2 == 0) {
@@ -31,7 +30,7 @@ function capitalize(s) {
       return letter.toLowerCase();
     }
   });
-  output.push(evens.join(''));
+  output.push(evens.join(""));
 
   let odds = split.map((letter, i) => {
     if (i % 2 != 0) {
@@ -40,7 +39,21 @@ function capitalize(s) {
       return letter.toLowerCase();
     }
   });
-  output.push(odds.join(''));
+  output.push(odds.join(""));
 
-  return output
+  return output;
+}
+
+// 6/27/2020
+
+// You are going to be given a word.Your job is to return the middle character of the word.If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters.
+
+function getMiddle(s) {
+  let length = s.length;
+  let index = Math.floor(length / 2);
+  if (length % 2 == 0) {
+    return s.slice(index - 1, index + 1);
+  } else if (length % 2 != 0) {
+    return s.slice(index, index + 1);
+  }
 }
