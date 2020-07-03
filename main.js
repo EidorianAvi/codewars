d72dc0f2107f25b8d64024a238d6a4bb927859e7
EidorianAvi <reneavila1993@gmail.com>
2020-07-03 12:16:39 -0600

    07/03/2020

--- main.js
+++ main.js
@@ -148,3 +148,21 @@ function likes(names) {
       return `${names[0]}, ${names[1]} and ${arrayLength - 2} others like this`;
   }
 }
+
+// 07/03/2020
+
+// In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+
+//   Example:
+
+// highAndLow("1 2 3 4 5");  // return "5 1"
+// highAndLow("1 2 -3 4 5"); // return "5 -3"
+// highAndLow("1 9 3 4 -5"); // return "9 -5"
+
+function highAndLow(numbers) {
+  let splitArray = numbers.split(" ");
+  let output = [];
+  let high = output.push(splitArray.reduce((a, b) => Math.max(a, b)));
+  let low = output.push(splitArray.reduce((a, b) => Math.min(a, b)));
+  return output.join(" ");
+}
