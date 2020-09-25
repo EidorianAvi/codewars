45ab6207156cb35fa2cc52a8024215cb98a10dd6
Adrian <reneavila1993@gmail.com>
2020-09-25 16:38:09 -0600

    09/25/22 second kata solved

--- main.js
+++ main.js
@@ -188,3 +188,27 @@ function persistence(num) {
 
   return 1 + persistence(product);
 }
+
+// Count the number of Duplicates
+// Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that occur more than once in the input string. The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.
+
+// Example
+// "abcde" -> 0 # no characters repeats more than once
+// "aabbcde" -> 2 # 'a' and 'b'
+// "aabBcde" -> 2 # 'a' occurs twice and 'b' twice (`b` and `B`)
+// "indivisibility" -> 1 # 'i' occurs six times
+// "Indivisibilities" -> 2 # 'i' occurs seven times and 's' occurs twice
+// "aA11" -> 2 # 'a' and '1'
+// "ABBA" -> 2 # 'A' and 'B' each occur twice
+
+function duplicateCount(text){
+  let duplicates = 0;
+  const characters = text.toLowerCase().split('').sort();
+  let letters = []
+  for( let i = 0; i < characters.length; i++){
+    if(characters[i] == characters[i + 1]) {
+        letters.push(characters[i]);
+    }
+  }
+  return [...new Set(letters)].length;
+}
