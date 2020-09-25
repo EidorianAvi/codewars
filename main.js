ef8c8fef46f1ec717828aaa9a2260917df35a4b2
Adrian <reneavila1993@gmail.com>
2020-09-25 14:53:40 -0600

    09/25/2020

--- main.js
+++ main.js
@@ -168,3 +168,23 @@ function highAndLow(numbers) {
 }
 
 
+// persistence(39) === 3 // because 3*9 = 27, 2*7 = 14, 1*4=4
+// // and 4 has only one digit
+
+// persistence(999) === 4 // because 9*9*9 = 729, 7*2*9 = 126,
+//  // 1*2*6 = 12, and finally 1*2 = 2
+
+// persistence(4) === 0 // because 4 is already a one-digit number
+
+function persistence(num) {
+  const numbers = num.toString().split('');
+  if (numbers.length === 1) {
+      return 0;
+  }
+
+  const product = numbers.reduce((product, digit) => {
+      return product * digit;
+  }, 1);
+
+  return 1 + persistence(product);
+}
