07a1f2e86ea30cb5561b19383e151da5d95da876
Adrian <reneavila1993@gmail.com>
2020-09-25 19:12:43 -0600

    Start next problem not yet solved

--- main.js
+++ main.js
@@ -257,3 +257,16 @@ function twoMovies(flightLength, movies){
 	}
 	return false;
 }
+
+// Task:
+// Your goal is to create a function dative() (Dative() in C#) which returns the valid form of a valid Hungarian word w in dative case i. e. append the correct suffix nek or nak to the word w based on vowel harmony rules.
+
+// Vowel Harmony Rules (simplified)
+// When the last vowel in the word is
+
+// a front vowel (e, é, i, í, ö, ő, ü, ű) the suffix is -nek
+// a back vowel (a, á, o, ó, u, ú) the suffix is -nak
+// Examples:
+// dative("ablak") == "ablaknak"
+// dative("szék") == "széknek"
+// dative("otthon") == "otthonnak"
