9335fc7bdccffa1d097feb99ede8505abff5990b
Adrian <reneavila1993@gmail.com>
2021-02-05 22:50:56 -0700

    02/05/21

--- main.js
+++ main.js
@@ -1311,3 +1311,16 @@ function extraLongFactorials(n) {
 
 }
 
+
+function jumpingOnClouds(c, k) {
+  let energy = 100;
+  let count = 0;
+  let index;
+  while (index !== 0 && energy >= 0) {
+    count === 0 && (index = 0);
+    index = (index + k) % c.length;
+    energy = c[index] > 0 ? energy - 3 : energy - 1;
+    count++;
+  }
+
+  r
