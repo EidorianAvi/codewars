40211d70c3d635e3b5d2399034cf468230a42bfa
Adrian <reneavila1993@gmail.com>
2021-02-08 13:21:49 -0700

    02/0/21

--- main.js
+++ main.js
@@ -1344,3 +1344,19 @@ function saveThePrisoner(n, m, s){
   return poisoned;
 
 }
+
+function processData(input) {
+  //Enter your code here
+
+  input = input.split('\n')
+  input[0] = input[0].split(' ')
+
+  let p = input[0][0] / input[0][1],
+    n = input[1]
+
+  function geometricDistribution(n, p) {
+    return Math.pow(1 - p, n - 1) * p
+  }
+
+  console.log(geometricDistribution(n, p).toFixed(3))
+}
