bf468ca9fa6d5c9973b2ecfe743fdbb3760fb462
Adrian <reneavila1993@gmail.com>
2021-03-16 05:26:41 -0600

    03/16/21

--- practice.rb
+++ practice.rb
@@ -35,4 +35,14 @@ def parts_sums(ls)
       sums.push(0)
     end
     sums
+  end
+
+
+# Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+# For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
+
+def square_sum(numbers)
+  squares = numbers.map{|number| number * number}
+  squares.reduce(0){|num, sum| num + sum}
 end
